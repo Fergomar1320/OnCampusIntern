418e7c7127d178c6c1862bb15420c3a5af3d86a3
Fergomar1320 <a01641228@tec.mx>
2024-05-20 01:07:35 -0600

    Teacher Model added.

---
+++ backend/models/teacher.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const teacherSchema = new Schema ({
+    name: { type: String, required: true, trim: true},
+    mail: { type: String, required: true, trim: true},
+    office: { type: String, required: true, trim: true},
+    area: { type: String, required: true, trim: true},
+    possition: { type: String, required: true, trim: true},
+}, {
+    timestamps: true,
+});
+
+const Teacher = mongoose.model('Teacher', teacherSchema)
+
+module.exports = Teacher;
