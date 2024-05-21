21283e826a283ffe8ad1ac0163ee0596b491702f
Fergomar1320 <a01641228@tec.mx>
2024-05-21 11:26:40 -0600

    Completed Teachers API endpoints

--- backend/routes/teachers.js
+++ backend/routes/teachers.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { response } = require('express');
 let Teacher = require('../models/teacher.model');
 
 router.route('/').get((req,res) =>{
@@ -27,6 +28,35 @@ router.route('/add').post((req, res) =>{
     newTeacher.save()
         .then(() => res.json('Teacher Added!'))
         .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+    Teacher.findById(req.params.id)
+    .then(teacher => res.json(teacher))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Teacher.findByIdAndDelete(req.params.id)
+    .then(teacher => res.json('Teacher deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Teacher.findById(req.params.id)
+    .then(teacher => {
+        teacher.name = req.body.name;
+        teacher.lastName = req.body.lastName;
+        teacher.mail = req.body.mail;
+        teacher.office = req.body.office;
+        teacher.area = req.body.area;
+        teacher.position = req.body.position;
+
+        teacher.save()
+            .then(() => res.json('Teacher updated!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
